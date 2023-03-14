2a9bdb3e6ab68aee64776d65c5f3b044e8c37033
ArtemOnopriienko <icartem.onopriienko@americanwell.com>
2023-03-15 00:53:34 +0200

    fix param name

--- src/app/admin/manage-products/manage-products.service.ts
+++ src/app/admin/manage-products/manage-products.service.ts
@@ -1,7 +1,7 @@
-import { Injectable, Injector } from '@angular/core';
-import { EMPTY, Observable } from 'rxjs';
-import { ApiService } from '../../core/api.service';
-import { switchMap } from 'rxjs/operators';
+import { Injectable, Injector } from "@angular/core";
+import { EMPTY, Observable } from "rxjs";
+import { ApiService } from "../../core/api.service";
+import { switchMap, tap } from "rxjs/operators";
 
 @Injectable()
 export class ManageProductsService extends ApiService {
@@ -10,31 +10,35 @@ export class ManageProductsService extends ApiService {
   }
 
   uploadProductsCSV(file: File): Observable<unknown> {
-    if (!this.endpointEnabled('import')) {
+    if (!this.endpointEnabled("import")) {
       console.warn(
-        'Endpoint "import" is disabled. To enable change your environment.ts config'
+        "Endpoint \"import\" is disabled. To enable change your environment.ts config"
       );
       return EMPTY;
     }
 
+    console.log('uploadProductsCSV file', file);
+
     return this.getPreSignedUrl(file.name).pipe(
-      switchMap((url) =>
-        this.http.put(url, file, {
-          headers: {
-            // eslint-disable-next-line @typescript-eslint/naming-convention
-            'Content-Type': 'text/csv',
-          },
-        })
+      switchMap((url) => {
+          return this.http.put(url, file, {
+            headers: {
+              // eslint-disable-next-line @typescript-eslint/naming-convention
+              "Content-Type": "text/csv"
+            }
+          });
+        }
       )
     );
   }
 
   private getPreSignedUrl(fileName: string): Observable<string> {
-    const url = this.getUrl('import', 'import');
+    const url = this.getUrl("import", "import")
+    console.log('fileName => ', fileName.split('.')[0]);
 
     return this.http.get<string>(url, {
       params: {
-        name: fileName,
+        name: fileName.split('.')[0]
       },
     });
   }
